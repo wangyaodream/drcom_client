033b796c1ca400d83b8b4f9a2573b10d6cb1e5d1
wangyao <wangyao.dream@hotmail.com>
2024-01-10 17:52:58 +0800

    add qthread function

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QLineEdit, QMessageBox
-from PyQt6.QtCore import QTimer
+from PyQt6.QtCore import QThread
 
+import drcom
+
+class Worker(QThread):
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        drcom.main()
 
 class SimpleApp(QWidget):
     def __init__(self):
@@ -12,20 +20,32 @@ class SimpleApp(QWidget):
         self.username_input = QLineEdit(self)
 
         password_label = QLabel("password", self)
-        password_input = QLineEdit(self)
-        password_input.setEchoMode(QLineEdit.EchoMode.Password)
+        self.password_input = QLineEdit(self)
+        self.password_input.setEchoMode(QLineEdit.EchoMode.Password)
 
         self.start_button = QPushButton("Start", self)
-        self.start_button.clicked.connect(self.show_info)
+        self.start_button.clicked.connect(self.start)
+
+        self.close_button = QPushButton("Close", self)
+        self.close_button.clicked.connect(QApplication.instance().quit)
 
         layout.addWidget(username_label)
         layout.addWidget(self.username_input)
         layout.addWidget(password_label)
-        layout.addWidget(password_input)
+        layout.addWidget(self.password_input)
         layout.addWidget(self.start_button)
+        layout.addWidget(self.close_button)
 
-    def show_info(self):
-        QMessageBox.information(self, "Hello!", self.username_input.text())
+        self.work_thread = Worker()
+
+
+    def start(self):
+        drcom.username = self.username_input.text()
+        drcom.password = self.password_input.text()
+        self.work_thread.start()
+
+    def get_username(self):
+        return self.username_input.text()
 
 
 if __name__ == "__main__":
